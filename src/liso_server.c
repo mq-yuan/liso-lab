3e04c2a67ef717c12896a444cd73890519c467ca
iLGZH <dearmengqiyuan@163.com>
2023-06-16 14:03:34 +0800

    fix bug that the last region is complete will not be parse

--- src/liso_server.c
+++ src/liso_server.c
@@ -94,6 +94,7 @@ int main(int argc, char *argv[]) {
   char fullpath[BUF_SIZE];
   char request_regions[MAX_REGIONS][BUF_SIZE];
   ssize_t base;
+  int flag;
 
   if (argc == 2) {
     echo_port = atoi(argv[1]);
@@ -156,19 +157,31 @@ int main(int argc, char *argv[]) {
     base = 0;
 
     while ((readret = recv(client_sock, buf + base, BUF_SIZE - base, 0)) >= 1) {
-      /* Parsing */
-      /* Request *request = parse(buf, BUF_SIZE, client_sock); */
+      /* split the buf */
       int regions_num = char_split(buf, "\r\n\r\n", request_regions);
 
-      for (int _r = 0; _r < regions_num - 1; _r++) {
+      /* mark whether last region is completed */
+      flag = 0;
+
+      for (int _r = 0; _r <= regions_num - 1; _r++) {
         char *token = request_regions[_r];
+        /* Parse */
         size_t n = (BUF_SIZE > strlen(token)) ? strlen(token) : BUFSIZ;
         Request *request = parse(token, n, client_sock);
+
         /* Generating the Response Messages for Unimplemented and Formatting
          * Errors*/
         /* For NULL */
         if (request == NULL) {
-          response_400(token, strlen(token), &readret);
+          if (_r == regions_num - 1) {
+            /* if the last region is not completed, set flag == 1
+             * if the lset region is completed, continue handle */
+            flag = 1;
+            memset(token, 0, BUF_SIZE);
+            break;
+          } else {
+            response_400(token, strlen(token), &readret);
+          }
         }
         /* For HTTP VERSION */
         else if (strncmp(request->http_version, "HTTP/1.1", 9) != 0) {
@@ -220,9 +233,18 @@ int main(int argc, char *argv[]) {
           free(request);
         }
       }
+
       memset(buf, 0, BUF_SIZE);
-      strcat(buf, request_regions[regions_num - 1]);
-      base = strlen(buf);
+
+      if (flag == 1) {
+        /* if the last region is not completed, move it to the front of buf
+         * and set base*/
+        strcat(buf, request_regions[regions_num - 1]);
+        base = strlen(buf);
+      } else {
+        /* if the last region is completed, set base is 0 */
+        base = 0;
+      }
     }
 
     /* ERROR Reading */
