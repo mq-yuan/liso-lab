8b416798fbedc060bf82c0d0dd6acdfb46c5c9a7
iLGZH <dearmengqiyuan@163.com>
2023-06-18 23:19:13 +0800

    do not know why it is success, but it is success

--- src/liso_server.c
+++ src/liso_server.c
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -89,11 +90,15 @@ int main(int argc, char *argv[]) {
   int echo_port;
   ssize_t readret;
   socklen_t cli_size;
-  struct sockaddr_in addr, cli_addr;
+  struct sockaddr_in cli_addr;
+  struct sockaddr_in addr[1024];
   char buf[BUF_SIZE];
   char fullpath[BUF_SIZE];
   char request_regions[MAX_REGIONS][BUF_SIZE];
   ssize_t base;
+  int max_fd;
+  int activity;
+  fd_set readfds;
 
   if (argc == 2) {
     echo_port = atoi(argv[1]);
@@ -111,9 +116,14 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(echo_port);
-  addr.sin_addr.s_addr = INADDR_ANY;
+  cli_addr.sin_family = AF_INET;
+  cli_addr.sin_port = htons(echo_port);
+  cli_addr.sin_addr.s_addr = INADDR_ANY;
+
+  addr[sock] = cli_addr;
+  FD_ZERO(&readfds);
+  FD_SET(sock, &readfds);
+  max_fd = sock;
 
   /* Allow address reuse */
   int reuse = 1;
@@ -123,9 +133,9 @@ int main(int argc, char *argv[]) {
   }
 
   /* servers bind sockets to ports---notify the OS they accept connections */
-  if (bind(sock, (struct sockaddr *)&addr, sizeof(addr))) {
+  if (bind(sock, (struct sockaddr *)&addr[sock], sizeof(addr[sock]))) {
     releasePort(echo_port);
-    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr))) {
+    if (bind(sock, (struct sockaddr *)&addr[sock], sizeof(addr[sock]))) {
       close_socket(sock);
       perror("Bind socket");
       fprintf(stderr, "Failed binding socket.\n");
@@ -143,108 +153,144 @@ int main(int argc, char *argv[]) {
 
   /* finally, loop waiting for input and then write it back */
   while (1) {
-    cli_size = sizeof(cli_addr);
-    if ((client_sock = accept(sock, (struct sockaddr *)&cli_addr, &cli_size)) ==
-        -1) {
-      close(sock);
-      fprintf(stderr, "Error accepting connection.\n");
-      errorLOG("Error accepting connection.\r\n");
-      return EXIT_FAILURE;
+    fd_set readset = readfds;
+    activity = select(FD_SETSIZE, &readset, NULL, NULL, NULL);
+    if ((activity < 0)) {
+      perror("Select error");
+    }
+    if (FD_ISSET(sock, &readset)) {
+      int newSocket;
+      cli_size = sizeof(cli_addr);
+      if ((newSocket = accept(sock, (struct sockaddr *)&cli_addr,
+                              (socklen_t *)&cli_size)) < 0) {
+        perror("Accept error");
+        exit(EXIT_FAILURE);
+      }
+      addr[newSocket] = cli_addr;
+      if (newSocket >= FD_SETSIZE) {
+        close(newSocket);
+        return EXIT_FAILURE;
+      }
+      FD_SET(newSocket, &readfds);
+      if (newSocket > max_fd) {
+        max_fd = newSocket;
+      }
+      if (--activity == 0)
+        continue;
     }
 
-    readret = 0;
-    base = 0;
+    /* cli_size = sizeof(cli_addr); */
+    /* if ((client_sock = */
+    /*          accept(sock, (struct sockaddr *)&cli_addr, &cli_size)) == -1) {
+     */
+    /*   close(sock); */
+    /*   fprintf(stderr, "Error accepting connection.\n"); */
+    /*   errorLOG("Error accepting connection.\r\n"); */
+    /*   return EXIT_FAILURE; */
+    /* } */
+    for (int i = sock + 1; i <= max_fd; i++) {
+      if (FD_ISSET(i, &readfds)) {
 
-    while ((readret = recv(client_sock, buf + base, BUF_SIZE - base, 0)) >= 1) {
-      /* split the buf */
-      int regions_num = char_split(buf, "\r\n\r\n", request_regions);
+        readret = 0;
+        base = 0;
+        client_sock = i;
 
-      /* mark whether last region is completed */
+        while ((readret = recv(client_sock, buf + base, BUF_SIZE - base, 0)) >=
+               1) {
+          /* split the buf */
+          int regions_num = char_split(buf, "\r\n\r\n", request_regions);
 
-      for (int _r = 0; _r < regions_num - 1; _r++) {
-        char *token = request_regions[_r];
-        /* Parse */
-        size_t n = (BUF_SIZE > strlen(token)) ? strlen(token) : BUFSIZ;
-        Request *request = parse(token, n, client_sock);
+          /* mark whether last region is completed */
 
-        /* Generating the Response Messages for Unimplemented and Formatting
-         * Errors*/
-        /* For NULL */
-        if (request == NULL) {
-          response_400(token, strlen(token), &readret);
-        }
-        /* For HTTP VERSION */
-        else if (strncmp(request->http_version, "HTTP/1.1", 9) != 0) {
-          response_505(token, strlen(token), &readret);
-        }
-        /* FOR NO IMPLEMENT */
-        else if ((strncmp(request->http_method, "GET", 4) != 0) &&
-                 (strncmp(request->http_method, "POST", 5) != 0) &&
-                 (strncmp(request->http_method, "HEAD", 5) != 0)) {
-          response_501(token, strlen(token), &readret);
-        }
-        /* FOR POST */
-        else if ((strncmp(request->http_method, "POST", 5) == 0)) {
-          char _token[LOG_BUF_SIZE];
-          sprintf(_token, " \"%s %s %s\" 200 ", request->http_method,
-                  request->http_uri, request->http_version);
-          accessLOG(_token);
-          memset(_token, 0, sizeof(_token));
-        }
-        /* FOR HEAD */
-        else if ((strncmp(request->http_method, "HEAD", 5) == 0)) {
-          response_head(fullpath, sizeof(fullpath), request, token,
-                        strlen(token), &readret);
-        }
-        /* FOR GET */
-        else if ((strncmp(request->http_method, "GET", 4) == 0)) {
-          response_get(fullpath, sizeof(fullpath), request, token,
-                       strlen(token), &readret);
-        }
+          for (int _r = 0; _r < regions_num - 1; _r++) {
+            char *token = request_regions[_r];
+            /* Parse */
+            size_t n = (BUF_SIZE > strlen(token)) ? strlen(token) : BUFSIZ;
+            Request *request = parse(token, n, client_sock);
 
-        /* SEND */
-        if ((request == NULL) ||
-            (strncmp(request->http_method, "GET", 4) != 0)) {
-          if (send_message(sock, client_sock, token) == 0) {
-            return EXIT_FAILURE;
+            /* Generating the Response Messages for Unimplemented and Formatting
+             * Errors*/
+            /* For NULL */
+            if (request == NULL) {
+              response_400(token, strlen(token), &readret);
+            }
+            /* For HTTP VERSION */
+            else if (strncmp(request->http_version, "HTTP/1.1", 9) != 0) {
+              response_505(token, strlen(token), &readret);
+            }
+            /* FOR NO IMPLEMENT */
+            else if ((strncmp(request->http_method, "GET", 4) != 0) &&
+                     (strncmp(request->http_method, "POST", 5) != 0) &&
+                     (strncmp(request->http_method, "HEAD", 5) != 0)) {
+              response_501(token, strlen(token), &readret);
+            }
+            /* FOR POST */
+            else if ((strncmp(request->http_method, "POST", 5) == 0)) {
+              char _token[LOG_BUF_SIZE];
+              sprintf(_token, " \"%s %s %s\" 200 ", request->http_method,
+                      request->http_uri, request->http_version);
+              accessLOG(_token);
+              memset(_token, 0, sizeof(_token));
+            }
+            /* FOR HEAD */
+            else if ((strncmp(request->http_method, "HEAD", 5) == 0)) {
+              response_head(fullpath, sizeof(fullpath), request, token,
+                            strlen(token), &readret);
+            }
+            /* FOR GET */
+            else if ((strncmp(request->http_method, "GET", 4) == 0)) {
+              response_get(fullpath, sizeof(fullpath), request, token,
+                           strlen(token), &readret);
+            }
+
+            /* SEND */
+            if ((request == NULL) ||
+                (strncmp(request->http_method, "GET", 4) != 0)) {
+              if (send_message(sock, client_sock, token) == 0) {
+                return EXIT_FAILURE;
+              }
+            } else {
+              if (handle_get_request(client_sock, fullpath, token) == 0) {
+                return EXIT_FAILURE;
+              }
+            }
+
+            memset(token, 0, BUF_SIZE);
+            memset(fullpath, 0, sizeof(fullpath));
+
+            /* free memory */
+            if (request != NULL) {
+              free(request->headers);
+              free(request);
+            }
           }
-        } else {
-          if (handle_get_request(client_sock, fullpath, token) == 0) {
-            return EXIT_FAILURE;
-          }
+
+          memset(buf, 0, BUF_SIZE);
+
+          strcat(buf, request_regions[regions_num - 1]);
+          base = strlen(buf);
+          break;
         }
 
-        memset(token, 0, BUF_SIZE);
-        memset(fullpath, 0, sizeof(fullpath));
+        /* ERROR Reading */
+        if (readret == -1) {
+          close_socket(client_sock);
+          close_socket(sock);
+          fprintf(stderr, "Error reading from client socket.\n");
+          errorLOG("Error reading from client socket.\r\n");
+          return EXIT_FAILURE;
+        }
 
-        /* free memory */
-        if (request != NULL) {
-          free(request->headers);
-          free(request);
+        /* ERROR CLOSING SOCKET */
+        if (close_socket(client_sock)) {
+          close_socket(sock);
+          fprintf(stderr, "Error closing client socket.\n");
+          errorLOG("Error closing client socket.\r\n");
+          return EXIT_FAILURE;
         }
       }
-
-      memset(buf, 0, BUF_SIZE);
-
-      strcat(buf, request_regions[regions_num - 1]);
-      base = strlen(buf);
-    }
-
-    /* ERROR Reading */
-    if (readret == -1) {
-      close_socket(client_sock);
-      close_socket(sock);
-      fprintf(stderr, "Error reading from client socket.\n");
-      errorLOG("Error reading from client socket.\r\n");
-      return EXIT_FAILURE;
-    }
-
-    /* ERROR CLOSING SOCKET */
-    if (close_socket(client_sock)) {
-      close_socket(sock);
-      fprintf(stderr, "Error closing client socket.\n");
-      errorLOG("Error closing client socket.\r\n");
-      return EXIT_FAILURE;
+      if (--activity == 0)
+        break;
     }
   }
 
